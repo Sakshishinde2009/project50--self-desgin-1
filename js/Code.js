7f97533363250bea9ddbd48e33ca8a2e12245aee
Sakshishinde2009 <119950720+Sakshishinde2009@users.noreply.github.com>
2023-05-02 21:13:24 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "REALTIME";
+const accessCode2 = "REF";
+const accessCode3 = "TESTMODE";
+
+function clues() {
+    
+    fill("white")
+    textSize(20)
+    text("I T E M R E A L ", 100,130)
+    fill("white")
+    text("Hint: It has parent-child modes !!", 100,150)
+
+    fill("white")
+    textSize(20)
+    text("F R E ", 650,180)
+    fill("white")
+    text("Hint: Function used to refer database !!", 650,210)
+
+    fill("white")
+    textSize(20)
+    text("O D M E T T S E ", 100,330)
+    fill("white")
+    text("Hint: The mode used for testing  !!", 100,350)
+
+}
